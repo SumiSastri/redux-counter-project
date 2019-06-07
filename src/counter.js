6978ac7f37e17af4f8373ce9c5c7c08a5a7faa4d
Your Name <sumisastri@gmail.com>
2019-06-07 20:10:20 +0100

    decrement added

--- src/counter.js
+++ src/counter.js
@@ -8,10 +8,14 @@ function Counter(props) {
 			<h1>Redux-React Counter</h1>
 			<p>Count ={props.count}</p>
 			<button onClick={props.onIncrementClick}>Increment</button>
-			<button>Decrement</button>
+			<button onClick={props.onDecrementClick}>Decrement</button>
 		</div>
 	);
 }
+// when state changes in the store we put it in this function
+// note the console log updates this when the state changes
+// type changes from redux init to increment the new action
+
 function mapStateToProps(state) {
 	console.log('mapStateToProps', state);
 	return {
@@ -19,17 +23,25 @@ function mapStateToProps(state) {
 	};
 }
 
-// set up second action - payload transfer from component to store (dispatcher)
+// Step 4 Dispatch set up second action - payload transfer from component to store (dispatcher)
+// note the console logs only once when the dispatch action is successfully fired
+// add second dispatch as it is another object add a comma
 
 function mapDispatchToProps(dispatch) {
+	console.log('mapDispatchToProps', dispatch);
 	return {
 		onIncrementClick: () => {
 			console.log('incrementClickWorking');
 			const action = { type: 'INCREMENT' };
 			dispatch(action);
+		},
+		onDecrementClick: () => {
+			console.log('decrementClickWorking');
+			const action = { type: 'DECREMENT' };
+			dispatch(action);
 		}
 	};
 }
-// connect dispatch actions to store
-
+// connect dispatch actions to store Step 3
+// wrap the component with the functions that you are mapping to connect to store
 export default connect(mapStateToProps, mapDispatchToProps)(Counter);
--- src/store/index.js
+++ src/store/index.js
@@ -6,18 +6,22 @@ const initialState = {
 	count: 0
 };
 
-// create reducer function in store - Step 2
+// create reducer function in store - Step 2 - it will have redux init once it is running
 // set up the reducer param - action in counter.js
+// always return state from the reducer
 
 const reducer = (state = initialState, action) => {
 	console.log('reducer running', action);
 
 	// set up the conditions of the action to be dispatched - here add 1 to original state
 	// object assign to brand new object to copy it - like a spread operator
+	// set up first action (creator)
 
 	switch (action.type) {
 		case 'INCREMENT':
 			return Object.assign({}, state, { count: state.count + 1 });
+		case 'DECREMENT':
+			return Object.assign({}, state, { count: state.count - 1 });
 		default:
 			return state;
 	}
